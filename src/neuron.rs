f55d0fd4e0572b8bab4356b1b8ea154a19044b92
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-03-27 19:36:23 +0300

    lib

---
+++ src/neuron.rs
@@ -0,0 +1,23 @@
+// trait Neuroner {}
+//
+// impl<T> Neuroner for Neuron<T> {}
+//
+// struct Neuron<T> {
+//     value: T, // Neuron value
+//     axon: [T], // All incoming axons
+//               //specific: Neuroner,  // Specific option of neuron: miss (error) or other
+// }
+
+// Neuroner
+// type Neuroner interface {
+// pkg.GetSetter
+// }
+//
+// // neuron
+// type neuron struct {
+//     value    floatType // Neuron value
+//     axon     []*axon   // All incoming axons
+//     specific Neuroner  // Specific option of neuron: miss (error) or other
+//
+//     Synapser
+// }
