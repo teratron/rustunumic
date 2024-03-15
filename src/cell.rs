bcb7e6e2a8aae1485358d222c73bb3daab2bf076
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-03-15 20:19:05 +0200

    Update cell.rs

--- src/cell.rs
+++ src/cell.rs
@@ -1,23 +1,58 @@
 struct Axon {
     weight: f32,
     incoming_cell: InCell,
+    // InCell, InputCell, BiasCell
     outgoing_cell: OutCell,
+    // OutCell, TargetCell
 }
+
+impl Axon {
+    fn calculate_value(&self, cell: &mut InCell) {
+        cell.value += self.incoming_cell.value * self.weight;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 struct CoreCell {
     activation_function: fn(f32) -> f32,
-    bias: bool,
 }
+
+//////////////////////////////////////////////////////////////////////////
 
 struct InCell {
     value: f32,
     incoming_axons: Vec<Axon>,
 }
 
+impl InCell {
+    fn calculate_value(&mut self) {
+        self.value = 0.;
+        for axon in &self.incoming_axons {
+            //self.value += axon.calculate_forward_value();
+            axon.calculate_value(self);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
 struct OutCell {
     miss: f32,
     outgoing_axons: Vec<Axon>,
 }
+
+impl OutCell {
+    fn calculate_miss(&mut self) {
+        for axon in &self.incoming_axons {
+            axon.calculate_value(self);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+struct BiasCell(bool);
 
 struct InputCell(f32);
 
