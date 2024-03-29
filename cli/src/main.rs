d26c53a7d48577851428a41b1488763dce6baf96
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-03-29 14:16:10 +0200

    Dotenv init

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,13 @@
 #![crate_type = "bin"]
 #![crate_name = "rustunumic"]
 
+use std::env;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let _ = dotenv_vault::dotenv();
 
-    let hello = std::env::var("HELLO").unwrap_or("".to_string());
+    let hello = env::var("HELLO").unwrap_or("".to_string());
     println!("Hello {hello}");
 
     Ok(())
