e3330c96c7295d6e2fe55e5798c3822ee9f921f7
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-04-05 12:13:33 +0300

    Update query.rs

--- src/query.rs
+++ src/query.rs
@@ -1,12 +1,34 @@
-//! # Querying
+//! # Query
 //!
 //!
 
 use super::{Float, Rustunumic};
 
 impl<T: Float> Rustunumic<T> {
+    /// Querying dataset.
     pub fn query(&mut self, input: &[T]) -> Vec<T> {
         let output: Vec<T> = vec![T::ZERO; 10];
+
+        if !self.is_init {
+            //raise ValueError(f"{__name__}: not initialized")
+        }
+        //self.input = input;
+        self.calculate_values();
+        //self.is_query = true
+
         output
     }
 }
+/*
+    def query(self, data_input: list[float]) -> list[float]:
+        """Querying dataset."""
+        if not self._params.is_init:
+            raise ValueError(f"{__name__}: not initialized")
+
+        self._data_input = data_input
+        self._calculate_neurons()
+        # self._params.is_query = True
+        self.__is_query = True
+
+        return [n.value for n in self._neurons[self._params.last_ind]]
+*/
--- src/verify.rs
+++ src/verify.rs
@@ -1,10 +1,10 @@
-//! # Verifying
-//!
+//! # Verify
 //!
 
 use super::{Float, Rustunumic};
 
 impl<T: Float> Rustunumic<T> {
+    /// Verifying dataset.
     pub fn verify(&mut self, input: &[T], target: &[T]) -> T {
         //let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
         //let slice = &numbers[1..5];
