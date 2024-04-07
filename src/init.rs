9dcc146c619ee71caaee15d5306e1ad113fc11eb
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-04-07 10:35:51 +0300

    modified: src/init.rs modified: src/propagation.rs

--- src/init.rs
+++ src/init.rs
@@ -7,7 +7,9 @@ use super::{Float, Rustunumic};
 impl<T: Float> Rustunumic<T> {
     /// Initialization neural network.
     pub(super) fn init(&mut self, input: &[T], target: &[T]) -> bool {
-        self.is_init = true;
-        self.is_init
+        let number_input = input.len();
+        let number_output = target.len();
+        //self.is_init = true;
+        !self.is_init
     }
 }
--- src/propagation.rs
+++ src/propagation.rs
@@ -10,9 +10,12 @@ impl<T: Float> Rustunumic<T> {
 
     /// Calculating neuron's value.
     pub(super) fn calculate_values(&mut self) {
-        for neuron in self.network.iter_mut() {
+        /*for neuron in self.network.iter_mut() {
             neuron.calculate_value();
-        }
+        }*/
+        self.network
+            .iter_mut()
+            .for_each(|n| n.calculate_value())
     }
 
     /// Calculating and return the total error of the output neurons.
@@ -21,6 +24,12 @@ impl<T: Float> Rustunumic<T> {
         for output in self.output.neurons.iter_mut() {
             loss += get_loss(output.get_miss(), &self.loss_mode);
         }
+
+        self.output.neurons
+            .iter_mut()
+            .reduce(|acc, e| acc + get_loss(e.get_miss(), &self.loss_mode))
+            .unwrap();
+
         loss /= self.output.number_float;
         if self.loss_mode == Loss::RMSE {
             loss = loss.sqrt();
@@ -32,16 +41,23 @@ impl<T: Float> Rustunumic<T> {
 
     /// Calculating the error of neuron.
     pub(super) fn calculate_misses(&mut self) -> &mut Self {
-        for neuron in self.hidden.neurons.iter_mut().rev() {
+        /*for neuron in self.hidden.neurons.iter_mut().rev() {
             neuron.calculate_miss();
-        }
+        }*/
+        self.hidden.neurons
+            .iter_mut()
+            .rev()
+            .for_each(|n| n.calculate_miss());
         self
     }
 
     /// Update weights.
     pub(super) fn calculate_weights(&mut self) {
-        for neuron in self.network.iter_mut() {
+        /*for neuron in self.network.iter_mut() {
             neuron.calculate_weight(&self.rate);
-        }
+        }*/
+        self.network
+            .iter_mut()
+            .for_each(|n| n.calculate_weight(&self.rate));
     }
 }
