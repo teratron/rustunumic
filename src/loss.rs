b385b141a383c7051ba60590f5f71964db6d15d6
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-04-10 14:39:01 +0300

    total_loss

--- src/loss.rs
+++ src/loss.rs
@@ -2,7 +2,7 @@
 //!
 //!
 
-use super::Float;
+use super::{Float, Rustunumic};
 
 pub(super) const LOSS_LIMIT: f64 = 1E-10;
 
@@ -39,6 +39,29 @@ pub(super) fn get_loss<T: Float>(value: &T, mode: &Loss) -> T {
         Loss::Arctan => value.atan().powi(2),
         Loss::MSE | Loss::RMSE | _ => value.powi(2),
     }
+}
+
+fn total_loss<T: Float>(
+    func: fn(Rustunumic<T>) -> dyn Iterator<Item = T>,
+) -> fn(Rustunumic<T>) -> T {
+    let inner = |obj: Rustunumic<T>| -> T {
+        let mut loss: T = T::ZERO;
+        let mut count: T = T::ZERO;
+        let miss = func(obj).next().unwrap();
+
+        loss += get_loss(&miss, &obj.loss_mode);
+        count += T::ONE;
+
+        if count > T::ONE {
+            loss /= count;
+        }
+
+        if obj.loss_mode == Loss::RMSE {
+            loss = loss.sqrt();
+        }
+        loss
+    };
+    inner
 }
 
 #[cfg(test)]
